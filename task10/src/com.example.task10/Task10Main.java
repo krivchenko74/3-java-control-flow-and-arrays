33215144fa2eb29b7abc8facdf83c25516f5f01e
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2025-09-08 10:17:17 +0500

    task10 completed

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minValue = Integer.MAX_VALUE;
+        int minNumber = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] <= minValue) {
+                minValue = arr[i];
+                minNumber = i;
+            }
+        }
+        return minNumber;
     }
 
 }
